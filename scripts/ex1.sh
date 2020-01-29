2e0d9521426229be9eb63be872530e2a3b1858cd
chef-centos <gnandamuru@localhost.localdomain>
2020-01-29 17:36:57 -0500

    adding ex1 example solution.

---
+++ scripts/ex1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Display Environment Variables
+
+#Create a script that, when run, will display the following environment variables to the console:
+#USER
+#HOME
+#HISTCONTROL
+#TERM
+
+
+echo "This script will give us the environment information."
+echo "====================================================="
+
+echo "Hello UserName: $USER"
+echo "Your Home Directory is: $HOME"
+echo "Your History File will Ignore: $HISTCONTROL"
+echo "Your Terminal Session Type is: $TERM"
+
+
+
