e711cacbc2ea28736977ce732ae4cd4d9a253a36
chef-centos <gnandamuru@localhost.localdomain>
2020-01-30 10:41:18 -0500

    Displaying Environment Variables in a Script.

---
+++ scripts/env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Displaying Environment Variables in a Script
+
+echo "This script will give us environment information."
+echo "" # new line
+echo "Hello Username: $USER"
+echo ""
+echo "Your Home Directory is: $HOME"
+echo ""
+echo "Your History File Will Ignore: $HISTCONTROL"
+echo ""
+echo "Your Terminal Session Type is: $TERM"
+echo ""
