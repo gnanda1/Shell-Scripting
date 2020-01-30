4cf8b6861c7b715513ae85ab30104c376582c219
chef-centos <gnandamuru@localhost.localdomain>
2020-01-30 11:32:24 -0500

    Comment Types and Structure.

---
+++ scripts/comments.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Comment Types and Structure
+
+#This line is intended to be used as a general description of the script
+#and anything that it does
+
+clear # clears the screen
+
+#MYUSERNAME="Terry"  #the username of this application
+MYUSERNAME="Don" # the new username added later
+
+echo "We are using the default user called: $MYUSERNAME" #display to the console.
+
+DATETIMESTAMP=`date`
+
+echo "This is when script was run: $DATETIMESTAMP" #this is the timestamp of the run
