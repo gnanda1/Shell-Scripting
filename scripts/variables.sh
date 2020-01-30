7fbbefbd54bfc41c1ba7afef27059f7d968ccf90
chef-centos <gnandamuru@localhost.localdomain>
2020-01-30 11:05:01 -0500

    Setting and Using Variables in Scripts.

---
+++ scripts/variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Setting and Using Variables in Scripts
+
+MYUSERNAME="username"
+MYPASSWORD='password123'
+STARTOFSCRIPT=`date`
+
+echo "My login name for this application is: $MYUSERNAME"
+echo "My password for this application is: $MYPASSWORD"
+echo "I started this script at: $STARTOFSCRIPT"
+
+ENDOFSCRIPT=`date`
+
+echo "I ended this script at: $ENDOFSCRIPT"
