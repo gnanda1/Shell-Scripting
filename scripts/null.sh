c6841d55e3abff9e8039ae6993474a45a6c4ed2d
chef-centos <gnandamuru@localhost.localdomain>
2020-02-03 11:36:17 -0500

    Using /dev/null example shell script.

---
+++ scripts/null.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Create a simple script that does the following:
+
+#1.Echo a full sentence to the terminal
+#2.Echo a different full sentence, but redirect it to /dev/null
+
+# redirect to /dev/null example
+
+echo "This is displaying on the console."
+
+echo "This is going into the black hole" >> /dev/null
