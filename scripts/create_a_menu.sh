425a7bb9610173eadf61eca4528934496ac53f74
chef-centos <gnandamuru@localhost.localdomain>
2020-02-10 12:33:44 -0500

    Sample use cases.

---
+++ scripts/create_a_menu.sh
@@ -0,0 +1,28 @@
+#Using the dialog control from the course, develop a function inside a script that will display a menu containing at least four choices. Capture the indicated value using hte appropriate output to a file. Reading that file, test the value and display an appropriate message, different for each on.
+
+#!/bin/bash
+# demo of a dialog box that will display a menu
+# global variables / default values
+
+MENUBOX=${MENUBOX=dialog}
+
+# function declarations - start
+# function to display a simple menu
+
+funcDisplayDialogMenu () {
+  $MENUBOX --title "[ M A I N   M E N U ]" --menu "Use UP/DOWN Arrows to Move and Select or the Number of Your Choice and Enter" 15 45 4 1 "Display Hello World" 2 "Display Goodbye World" 3 "Display Nothing" X "Exit" 2>choice.txt
+}
+
+# function declarations - stop
+# script - start
+
+funcDisplayDialogMenu
+
+case "`cat choice.txt`" in
+  1) echo "Hello World";;
+  2) echo "Goodbye World";;
+  3) echo "Nothing";;
+  X) echo "Exit";;
+esac
+
+# script - stop
--- scripts/user_input_dialog.sh
+++ scripts/user_input_dialog.sh
@@ -1,29 +1,33 @@
-#Using the dialog control from the course, develop a function inside a script that will display a menu containing at least four choices. Capture the indicated value using hte appropriate output to a file. Reading that file, test the value and display an appropriate message, different for each on.
+#In this script, we will be using an Input Box constructed from the dialog control, to prompt the user for a filename to display to the terminal.
+#Construct the input box within a function and capture the value input using the appropriate output method to a file. Read that file back in and attempt to display (cat) the file to the terminal or indicate that it does not exist.
+
 
 #!/bin/bash
 
-# demo of a dialog box that will display a menu
+# simple demo of an input dialog box
 # global variables / default values
 
-MENUBOX=${MENUBOX=dialog}
+INPUTBOX=${INPUTBOX=dialog}
+TITLE="Default"
+MESSAGE="Something to display"
+XCOORD=10
+YCOORD=20
 
 # function declarations - start
-# function to display a simple menu
+# display the input box
 
-funcDisplayDialogMenu () {
-  $MENUBOX --title "[ M A I N   M E N U ]" --menu "Use UP/DOWN Arrows to Move and Select or the Number of Your Choice and Enter" 15 45 4 1 "Display Hello World" 2 "Display Goodbye World" 3 "Display Nothing" X "Exit" 2>choice.txt
+funcDisplayInputBox () {
+  $INPUTBOX --title "$1" --inputbox "$2" "$3" "$4" 2>tmpfile.txt
 }
+
 
 # function declarations - stop
 # script - start
 
-funcDisplayDialogMenu
 
-case "`cat choice.txt`" in
-  1) echo "Hello World";;
-  2) echo "Goodbye World";;
-  3) echo "Nothing";;
-  X) echo "Exit";;
-esac
-
-# script - stop
+funcDisplayInputBox "Display File Name" "Which file in the current directory do you want to display?" "10" "20"
+if [ "`cat tmpfile.txt`" != "" ]; then
+  cat "`cat tmpfile.txt`"
+else
+  echo "Nothing to do"
+fi
